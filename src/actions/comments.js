28e692a61ca10c7a816ab2b977c74ad175629f76
MarcioCantos <cantoscorrea@gmail.com>
2019-03-12 14:05:13 -0300

    Remoção constante de ordenação de comentários

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -1,45 +1,36 @@
 import {
-    REQUEST_COMMENTS_BY_POST,
-    ADD_COMMENT,
-    DELETE_COMMENT,
-    RATE_COMMENT,
-    SORT_COMMENT_BY,
-  } from './const'
+  REQUEST_COMMENTS_BY_POST,
+  ADD_COMMENT,
+  DELETE_COMMENT,
+  RATE_COMMENT,
+} from './const'
   
-  export function requestComentsByPost(id){
-    return {
-      type : REQUEST_COMMENTS_BY_POST,
-      id,
-    }
+export function requestComentsByPost(id){
+  return {
+    type : REQUEST_COMMENTS_BY_POST,
+    id,
   }
-  
-  export function addComments(comment, parentId){
-    return {
-      type : ADD_COMMENT,
-      comment,
-      parentId,
-    }
+}
+
+export function addComments(comment, parentId){
+  return {
+    type : ADD_COMMENT,
+    comment,
+    parentId,
   }
-  
-  export function deleteComment(id){
-    return {
-      type : DELETE_COMMENT,
-      id,
-    }
+}
+
+export function deleteComment(id){
+  return {
+    type : DELETE_COMMENT,
+    id,
   }
-  
-  export function rateComment(id, vote){
-    return {
-      type : RATE_COMMENT,
-      id,
-      vote,
-    }
+}
+
+export function rateComment(id, vote){
+  return {
+    type : RATE_COMMENT,
+    id,
+    vote,
   }
-  
-  export function sortComment(param, order){  
-    return {
-      type : SORT_COMMENT_BY,
-      column : param,
-      order,
-    }
-  }
+}
